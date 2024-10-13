e27c9ad0f2ab6c2d0265af6338e89195a60ef183
Ansh <[ansh96823@gmail.com]>
2024-10-14 00:08:48 +0530

    Refactor removeFromArray.js to remove multiple items from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,27 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...item) {
+    for( const n of item)arr= remove(arr,n);
+   return arr;
+};
+
+function searchItem(arr,item){
+    len= arr.length;
+    for(let i=0; i<  len; i++)
+    {
+        if(arr[i]== item && typeof arr[i]== typeof item ) return i;
+    }
+    return -1;
+}
+
+function remove(arr,item){
+   let pos = -1;
+   do{
+    if(item!=null){pos= searchItem(arr,item);
+        if (pos== -1) break;
+        arr.splice(pos,1);
+   }}while(pos!=-1);
+
+
+return arr;
 };
 
 // Do not edit below this line
